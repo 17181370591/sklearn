0c9fe1473e7ed710e7721cacdc55432dfd038fb3
17181370591 <34680371+17181370591@users.noreply.github.com>
2018-07-24 15:14:41 +0800

    Update 莫言2：make_regression生成数据并用LinearRegression测试.py

--- 莫言2：make_regression生成数据并用LinearRegression测试.py
+++ 莫言2：make_regression生成数据并用LinearRegression测试.py
@@ -1,5 +1,8 @@
 #https://www.bilibili.com/video/av17003173/?p=6
 #线性回归生成一个拟合函数，来预测新数据的值
+#model.coef_和model.intercept是什么不知道
+#model.score(x,y)可以打分，可以看到make_regression的noise越大，分数就越低
+
 
 import numpy as np
 from sklearn import datasets
@@ -23,6 +26,9 @@ x,y=datasets.make_regression(n_samples=100,n_features=1,n_targets=1,noise=5)    
 model=LinearRegression()
 model.fit(x,y)
 res=model.predict(x)
+
+print('model.coef_=',model.coef_,'model.intercept_=', model.intercept_)
+print(model.score(x,y))
 
 plt.scatter(x,y,color='r')
 plt.scatter(x,res)
